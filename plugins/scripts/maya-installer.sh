4ce729c00bf66fc2e2a290daa1a8e8f30d72be56
ddesmond <crni.aki@gmail.com>
2024-10-08 10:36:01 +0200

    add maya wip

--- plugins/scripts/maya-installer.sh
+++ plugins/scripts/maya-installer.sh
@@ -1,5 +1,4 @@
 echo "Installing Autodesk Maya..."
-
 temp_install_dir=$2/maya_installer
 
 mkdir -p $temp_install_dir
@@ -7,9 +6,7 @@ mkdir -p $2
 
 chmod -R 777 $temp_install_dir/
 chmod -R 777 $2
-
-cd /tmp
-
+cd /
 if [ "$DEV_APPS_DEBUG" = true ]
 then
   echo "Dev Apps Debug is enabled - maya copied with docker"
@@ -18,14 +15,9 @@ else
   wget -q -O /tmp/mayainstaller.tgz "$1"
 fi
 
-chmod +x /tmp/mayainstaller.tgz
-
-
-tar -xvf /tmp/mayainstaller.tgz -C $temp_install_dir
-
-chmod -R 777 $temp_install_dir/
-chmod -R 777 $2
-
+# extract the installer
+tar -xvzf /tmp/mayainstaller.tgz -C $temp_install_dir
 # check maya installer
+ls -la /tmp
 ls -la $temp_install_dir
 
