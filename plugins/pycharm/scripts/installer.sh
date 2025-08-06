38cf25de91214a567d8d82d754362f74d26c3959
dwalters <danjwalters@gmail.com>
2025-08-06 16:45:23 -0400

    updates per feedback

--- plugins/pycharm/scripts/installer.sh
+++ plugins/pycharm/scripts/installer.sh
@@ -1,33 +1,26 @@
 #!/bin/bash
 set -e
-# install wget
+# install curl
 apt update
-echo "installing wget"
-apt install -y wget
+apt install curl -y
 
 LAUNCH="$DESTINATION/pycharm-$VERSION/bin/pycharm.sh "
 ICON="$DESTINATION/pycharm.png"
 
-# store our current working dir
-working_dir="$PWD"
-echo $working_dir
-
 echo "Installing $VERSION"
 echo "Destination $DESTINATION"
 
-cd /tmp
-wget -q -O pycharm-$VERSION.tar.gz -P /tmp "https://download-cdn.jetbrains.com/python/pycharm-$VERSION.tar.gz"
-chmod +x /tmp/pycharm-$VERSION.tar.gz
+curl -o /tmp/pycharm-$VERSION.tar.gz -P /tmp "https://download-cdn.jetbrains.com/python/pycharm-$VERSION.tar.gz"
 
 echo "Extracting pycharm..."
 mkdir -p "$DESTINATION"
 tar xzvf /tmp/pycharm-$VERSION.tar.gz -C $DESTINATION/
-chmod -R 777 $DESTINATION
+chmod -R 555 $DESTINATION
 
 echo "Adding desktop files"
 # app icon setup
 cd $working_dir
-cp -v ./assets/pycharm.png $DESTINATION/
+cp -v ${PWD}/assets/pycharm.png $DESTINATION/
 rm -rfv "$DESTINATION/pycharm.desktop"
 
 echo "[Desktop Entry]
