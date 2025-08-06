f5caef8a8296c1002860d1b751a6686d41e66088
dwalters <danjwalters@gmail.com>
2025-08-06 17:48:42 -0400

    linter fixes

--- plugins/pycharm/scripts/installer.sh
+++ plugins/pycharm/scripts/installer.sh
@@ -11,16 +11,16 @@ ICON="$INSTALL_DIR/pycharm.png"
 echo "Installing $VERSION"
 echo "Destination $INSTALL_DIR"
 
-curl -o /tmp/pycharm-$VERSION.tar.gz -P /tmp "https://download-cdn.jetbrains.com/python/pycharm-$VERSION.tar.gz"
+curl -o "/tmp/pycharm-$VERSION.tar.gz" -P /tmp "https://download-cdn.jetbrains.com/python/pycharm-$VERSION.tar.gz"
 
 echo "Extracting pycharm..."
 mkdir -p "$INSTALL_DIR"
-tar xzvf /tmp/pycharm-$VERSION.tar.gz -C $DESTINATION/
-chmod -R 555 $DESTINATION
+tar xzvf "/tmp/pycharm-$VERSION.tar.gz" -C "$DESTINATION/"
+chmod -R 555 "$DESTINATION"
 
 echo "Adding desktop files"
 # app icon setup
-cp -v ${PWD}/assets/pycharm.png $INSTALL_DIR/
+cp -v "${PWD}/assets/pycharm.png" "$INSTALL_DIR/"
 rm -rfv "$INSTALL_DIR/pycharm.desktop"
 
 echo "[Desktop Entry]
@@ -31,6 +31,6 @@ Exec=$LAUNCH
 Icon=$ICON
 Terminal=true
 Type=Application
-Categories=X-Polaris" >> $INSTALL_DIR/pycharm.desktop
+Categories=X-Polaris" >> "$INSTALL_DIR"/pycharm.desktop
 
-cat $INSTALL_DIR/*.desktop
+cat "$INSTALL_DIR"/*.desktop
