0ec795f496f8dfb250f5a160714e6fe3fb801a51
dwalters <danjwalters@gmail.com>
2025-08-06 12:15:00 -0400

    pycharm clean up

--- plugins/pycharm/scripts/installer.sh
+++ plugins/pycharm/scripts/installer.sh
@@ -5,23 +5,20 @@ apt update
 echo "installing wget"
 apt install -y wget
 
-executable="$(echo "$VERSION" | cut -d'v' -f1)"
-LAUNCH="$DESTINATION/pycharm-community-$VERSION/$executable"
+LAUNCH="$DESTINATION/$VERSION/$executable"
 ICON="$DESTINATION/pycharm.png"
 
 echo "Installing $VERSION"
 echo "Destination $DESTINATION"
-echo "Executable: $executable"
 
 cd /tmp
-
-wget -q -O /tmp/pycharm-community-$VERSION "https://download.jetbrains.com/python/pycharm-community-$VERSION.tar.gz"
-chmod +x /tmp/pycharm-community-$VERSION.tar.gz
+wget -q -O /tmp/pycharm-$VERSION "https://download-cdn.jetbrains.com/python/pycharm-$VERSION.tar.gz"
+chmod +x /tmp/pycharm-$VERSION.tar.gz
 
 echo "Extracting pycharm..."
-tar xzvf /tmp/pycharm-community-$VERSION.tar.gz -C $DESTINATION/
+tar xzvf /tmp/pycharm-$VERSION.tar.gz -C $DESTINATION/
 
-chmod -R 777 $DESTINATION/pycharm-community-$VERSION
+chmod -R 777 $DESTINATION/$VERSION
 chmod -R 777 $DESTINATION
 
 # app icon setup
@@ -29,9 +26,9 @@ cp -v ./assets/pycharm.png $DESTINATION/
 rm -rfv "$DESTINATION/pycharm.desktop"
 
 echo "[Desktop Entry]
-Version=$DESTINATION
-Name=Pycharm Community $DESTINATION
-Comment=Pycharm Community IDE
+Version=$VERSION
+Name=Pycharm $VERSION
+Comment=Pycharm IDE
 Exec=$LAUNCH
 Icon=$ICON
 Terminal=true
