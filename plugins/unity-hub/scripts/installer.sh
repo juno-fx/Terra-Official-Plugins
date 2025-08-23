5a1b5040ba8715d6b36c3effbf3df3b9ec1c65f1
dwalters <dwalters@junovfx.com>
2025-08-23 17:46:54 -0400

    fixing installer

--- plugins/unity-hub/scripts/installer.sh
+++ plugins/unity-hub/scripts/installer.sh
@@ -1,7 +1,12 @@
 set -e
 # https://docs.unity3d.com/hub/manual/InstallHub.html
+
+echo "Installing needed packages"
+apt install -y wget
+apt install -y gpg
+
 echo "installing Unity Hub"
 wget -qO - https://hub.unity3d.com/linux/keys/public | gpg --dearmor | sudo tee /usr/share/keyrings/Unity_Technologies_ApS.gpg > /dev/null
-sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/Unity_Technologies_ApS.gpg] https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list'
-sudo apt update
-sudo apt-get install unityhub
+sh -c 'echo "deb [signed-by=/usr/share/keyrings/Unity_Technologies_ApS.gpg] https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list'
+apt update
+apt-get install unityhub
