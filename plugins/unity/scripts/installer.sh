5b473a9c9b5d14ca4312859ef1393e547895461e
dwalters <dwalters@junovfx.com>
2025-08-23 16:09:43 -0400

    fixed tar command

--- plugins/unity/scripts/installer.sh
+++ plugins/unity/scripts/installer.sh
@@ -23,7 +23,7 @@ curl -o "/tmp/unity-$VERSION.tar.xz" -P /tmp "$url"
 
 echo "Extracting Unity..."
 mkdir -p "$INSTALL_DIR"
-tar xzvf "/tmp/unity-$VERSION.tar.xz" -C "$INSTALL_DIR/"
+tar xJvf "/tmp/unity-$VERSION.tar.xz" -C "$INSTALL_DIR/"
 chmod -R 555 "$DESTINATION"
 
 echo "Adding desktop files"
