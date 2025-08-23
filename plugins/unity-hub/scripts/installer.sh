f2bb97c7a82e5a9256c2009be6724f295b69ad93
dwalters <dwalters@junovfx.com>
2025-08-23 17:49:11 -0400

    removing sudo

--- plugins/unity-hub/scripts/installer.sh
+++ plugins/unity-hub/scripts/installer.sh
@@ -7,7 +7,7 @@ apt install -y wget
 apt install -y gpg
 
 echo "installing Unity Hub"
-wget -qO - https://hub.unity3d.com/linux/keys/public | gpg --dearmor | sudo tee /usr/share/keyrings/Unity_Technologies_ApS.gpg > /dev/null
+wget -qO - https://hub.unity3d.com/linux/keys/public | gpg --dearmor | tee /usr/share/keyrings/Unity_Technologies_ApS.gpg > /dev/null
 sh -c 'echo "deb [signed-by=/usr/share/keyrings/Unity_Technologies_ApS.gpg] https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list'
 apt update
 apt-get install unityhub
