ba0692c080a698913bc9d67dd3bafb499f711c16
dwalters <dwalters@junovfx.com>
2025-08-28 22:10:10 -0400

    debugging

--- plugins/houdini/scripts/installer.sh
+++ plugins/houdini/scripts/installer.sh
@@ -45,7 +45,7 @@ venv/bin/python "$WORKING_DIR/sidefx_downloader.py" --version $HOUDINI_VERSION -
 echo "Extracting houdini-launcher.iso"
 
 chmod 555 "$TEMP_VERSION_FOLDER"/houdini-launcher.iso
-7z x "$TEMP_VERSION_FOLDER"/houdini-launcher.iso -o"$TEMP_VERSION_FOLDER"/installs > "$TEMP_VERSION_FOLDER"/houdini_extract.log
+7z x "$TEMP_VERSION_FOLDER"/houdini-launcher.iso -o"$TEMP_VERSION_FOLDER"
 
 echo "houdini-launcher.iso extracted to "$TEMP_VERSION_FOLDER""
 echo "Installing Houdini Launcher... "$INSTALL_DIR"/launcher ..."
@@ -60,8 +60,10 @@ mkdir -p "$INSTALL_DIR/shfs"
 chmod -R 555 "$INSTALL_DIR/shfs"
 echo "Houdini Install Dir: "$INSTALL_DIR"/launcher"
 
-cd "$TEMP_VERSION_FOLDER"/installs
+cd "$TEMP_VERSION_FOLDER"
+echo $PWD
 ls
+
 ./install_houdini.launcher.sh "$INSTALL_DIR"/launcher
 
 echo "License Date:" $LICENSE_DATE
