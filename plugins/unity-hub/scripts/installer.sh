cc9f3ac0fa046b9cee7af103d77921543422c936
dwalters <dwalters@junovfx.com>
2025-09-03 16:35:09 -0400

    setting up unity hub

---
+++ plugins/unity-hub/scripts/installer.sh
@@ -0,0 +1,49 @@
+set -e
+
+echo "installing needed packages"
+apt update
+apt install wget -y
+INSTALL_DIR="$DESTINATION/unity-hub"
+LAUNCH="$INSTALL_DIR/unityhub"
+ICON="$INSTALL_DIR/unity.png"
+TEMP_LOCATION='/tmp/unity-hub'
+
+echo "Destination $INSTALL_DIR"
+
+wget -qO - https://hub.unity3d.com/linux/keys/public | gpg --dearmor | sudo tee /usr/share/keyrings/Unity_Technologies_ApS.gpg > /dev/null
+
+sh -c 'echo "deb [signed-by=/usr/share/keyrings/Unity_Technologies_ApS.gpg] https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list'
+
+URL=$(apt-get -qq --print-uris download unityhub)
+
+echo "Downloading Unity Hub"
+echo $URL
+
+mkdir -p "$TEMP_LOCATION"
+wget -O "$TEMP_LOCATION/unity-hub.deb" "$URL"
+
+echo "Extracting Unity Hub..."
+mkdir -p $INSTALL_DIR
+dpkg-deb -xv "$TEMP_LOCATION/unity-hub.deb" "$TEMP_LOCATION"
+
+tar xzvf "$TEMP_LOCATION/data.tar.gz"-C "$INSTALL_DIR/"
+chmod -R 555 "$INSTALL_DIR"
+
+echo "Adding desktop files"
+
+# app icon setup
+cp -v "${PWD}/assets/unity.png" "$INSTALL_DIR/"
+rm -rfv "$INSTALL_DIR/unity.desktop"
+
+echo "[Desktop Entry]
+Name=unity Hub
+Comment=Unity Hub
+Exec=$LAUNCH
+Icon=$ICON
+Terminal=true
+Type=Application
+Categories=X-Polaris" >> "$INSTALL_DIR"/unity.desktop
+
+
+cat "$INSTALL_DIR"/*.desktop
+
