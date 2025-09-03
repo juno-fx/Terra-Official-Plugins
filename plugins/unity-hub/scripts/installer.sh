f426784772156b7dc244358343004a01f7653e6a
dwalters <dwalters@junovfx.com>
2025-09-03 16:39:13 -0400

    debugging

--- plugins/unity-hub/scripts/installer.sh
+++ plugins/unity-hub/scripts/installer.sh
@@ -3,6 +3,7 @@ set -e
 echo "installing needed packages"
 apt update
 apt install wget -y
+apt install gnupg -y
 INSTALL_DIR="$DESTINATION/unity-hub"
 LAUNCH="$INSTALL_DIR/unityhub"
 ICON="$INSTALL_DIR/unity.png"
@@ -10,7 +11,7 @@ TEMP_LOCATION='/tmp/unity-hub'
 
 echo "Destination $INSTALL_DIR"
 
-wget -qO - https://hub.unity3d.com/linux/keys/public | gpg --dearmor | sudo tee /usr/share/keyrings/Unity_Technologies_ApS.gpg > /dev/null
+wget -qO - https://hub.unity3d.com/linux/keys/public | gpg --dearmor | tee /usr/share/keyrings/Unity_Technologies_ApS.gpg > /dev/null
 
 sh -c 'echo "deb [signed-by=/usr/share/keyrings/Unity_Technologies_ApS.gpg] https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list'
 
