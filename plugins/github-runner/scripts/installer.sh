13968e3409f2a0c119020e2150517b80153e6246
aldmbmtl <alex.e.hatfield@gmail.com>
2025-10-20 20:48:51 -0400

    Pushing first startup

--- plugins/github-runner/scripts/installer.sh
+++ plugins/github-runner/scripts/installer.sh
@@ -1,23 +1,24 @@
 set -e
 
-chroot /host ls -la
+apt update
+apt install -y \
+  curl \
+  tar
 
-#apt update
-#apt install -y \
-#  curl \
-#  tar
-#
-## setup workspace
-#mkdir -pv ${DESTINATION}
-#cd ${DESTINATION}
-#
-## download the runner package
-#echo "curl -o runner.tar.gz -L https://github.com/actions/runner/releases/download/v${VERSION}/actions-runner-linux-${ARCHITECTURE}-${VERSION}.tar.gz"
-#curl -o runner.tar.gz -L "https://github.com/actions/runner/releases/download/v${VERSION}/actions-runner-linux-${ARCHITECTURE}-${VERSION}.tar.gz"
-#
-## extract the runner package
-#tar -xzf runner.tar.gz
-#
-## configure the runner
-#./bin/installdependencies.sh
-#RUNNER_ALLOW_RUNASROOT="1" ./config.sh --unattended --url "${REPO_URL}" --token "${TOKEN}" --name "${NAME}"
+# setup workspace
+mkdir -pv /host/${DESTINATION}/${NAME}
+cd /host/${DESTINATION}/${NAME}
+
+# download the runner package
+echo "curl -o runner.tar.gz -L https://github.com/actions/runner/releases/download/v${VERSION}/actions-runner-linux-${ARCHITECTURE}-${VERSION}.tar.gz"
+curl -o runner.tar.gz -L "https://github.com/actions/runner/releases/download/v${VERSION}/actions-runner-linux-${ARCHITECTURE}-${VERSION}.tar.gz"
+
+# extract the runner package
+tar -xzf runner.tar.gz
+
+# install provision script
+cp /terra/run.sh ./provision.sh
+chmod +x ./provision.sh
+
+# chroot and run provision script
+chroot /host/ bash -c "cd /${DESTINATION}/${NAME} && RUNNER_ALLOW_RUNASROOT='1' REPO_URL=${REPO_URL} TOKEN=${TOKEN} NAME=${NAME} ./provision.sh"
---
+++ plugins/github-runner/scripts/provision.sh
@@ -0,0 +1,2 @@
+# configure the runner
+./config.sh --unattended --url "${REPO_URL}" --token "${TOKEN}" --name "${NAME}"
