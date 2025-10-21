8a495c9573f646c4874ea8d884c866fc54695bfb
aldmbmtl <alex.e.hatfield@gmail.com>
2025-10-20 21:12:46 -0400

    Pushing first startup

--- plugins/github-runner/scripts/provision.sh
+++ plugins/github-runner/scripts/provision.sh
@@ -5,5 +5,4 @@ set -e
 echo "./config.sh --unattended --url ${URL} --token ${TOKEN} --name ${NAME}"
 
 # configure the runner
-./config.sh remove || true
 ./config.sh --unattended --url ${URL} --token ${TOKEN} --name ${NAME} --replace
