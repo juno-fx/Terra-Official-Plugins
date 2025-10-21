17d8d79d2446128037a3abc486ed4096c2dd546f
aldmbmtl <alex.e.hatfield@gmail.com>
2025-10-20 21:31:33 -0400

    Simplifying the process

--- plugins/github-runner/scripts/entrypoint.sh
+++ plugins/github-runner/scripts/entrypoint.sh
@@ -6,6 +6,48 @@ if [ -n "$CLEANUP" ]; then
   exit 0
 fi
 
+# install required tools
+apt update
+apt install -y \
+  curl \
+  tar \
+  rsync
+
+# clean up any previous installation
+rm -rf /host/${DESTINATION}/${NAME}
+mkdir -pv /host/${DESTINATION}/${NAME}
+
+# fresh runner install
 if [ -n "${INSTALL}" ]; then
-    ./installer.sh
+    # clean previous network install
+    rm -rf /${DESTINATION}/${NAME}
+
+    # change to workspace
+    cd /host/${DESTINATION}/${NAME}
+
+    # download the runner package
+    echo "curl -o runner.tar.gz -L https://github.com/actions/runner/releases/download/v${VERSION}/actions-runner-linux-${ARCHITECTURE}-${VERSION}.tar.gz"
+    curl -o runner.tar.gz -L "https://github.com/actions/runner/releases/download/v${VERSION}/actions-runner-linux-${ARCHITECTURE}-${VERSION}.tar.gz"
+
+    # extract the runner package
+    tar -xzf runner.tar.gz
+
+    # chroot and run provision script
+    chroot /host/ bash -c "cd /${DESTINATION}/${NAME} && RUNNER_ALLOW_RUNASROOT='1' ./config.sh --unattended --url ${URL} --token ${TOKEN} --name ${NAME} --replace"
+
+    echo "Copying runner configuration to shared storage..."
+    rsync -a /host/${DESTINATION}/${NAME}/ /${DESTINATION}/${NAME}/
+    echo "GitHub Actions Runner v${VERSION} installed at ${DESTINATION}/${NAME}"
+
+    exit 0
+fi
+
+if [ -n "${LAUNCH}" ]; then
+    # pull the runners configuration from shared storage
+    rsync -a /${DESTINATION}/${NAME}/ /host/${DESTINATION}/${NAME}/
+
+    # chroot and run the run.sh script
+    chroot /host/ bash -c "cd /${DESTINATION}/${NAME} && ./run.sh"
+
+    exit 0
 fi
--- plugins/github-runner/scripts/provision.sh
+++
@@ -1,8 +0,0 @@
-# echo all configurations
-
-set -e
-
-echo "./config.sh --unattended --url ${URL} --token ${TOKEN} --name ${NAME}"
-
-# configure the runner
-./config.sh --unattended --url ${URL} --token ${TOKEN} --name ${NAME} --replace
