fdbf698cd09f7ca507c7676f36d42fcd49fadc41
aldmbmtl <alex.e.hatfield@gmail.com>
2025-10-20 20:37:08 -0400

    Pushing first startup

--- plugins/github-runner/scripts/installer.sh
+++ plugins/github-runner/scripts/installer.sh
@@ -1,21 +1,23 @@
 set -e
 
-apt update
-apt install -y \
-  curl \
-  tar
+chroot /host ls -la
 
-# setup workspace
-mkdir -pv ${DESTINATION}
-cd ${DESTINATION}
-
-# download the runner package
-echo "curl -o runner.tar.gz -L https://github.com/actions/runner/releases/download/v${VERSION}/actions-runner-linux-${ARCHITECTURE}-${VERSION}.tar.gz"
-curl -o runner.tar.gz -L "https://github.com/actions/runner/releases/download/v${VERSION}/actions-runner-linux-${ARCHITECTURE}-${VERSION}.tar.gz"
-
-# extract the runner package
-tar -xzf runner.tar.gz
-
-# configure the runner
-./bin/installdependencies.sh
-RUNNER_ALLOW_RUNASROOT="1" ./config.sh --unattended --url "${REPO_URL}" --token "${TOKEN}" --name "${NAME}"
+#apt update
+#apt install -y \
+#  curl \
+#  tar
+#
+## setup workspace
+#mkdir -pv ${DESTINATION}
+#cd ${DESTINATION}
+#
+## download the runner package
+#echo "curl -o runner.tar.gz -L https://github.com/actions/runner/releases/download/v${VERSION}/actions-runner-linux-${ARCHITECTURE}-${VERSION}.tar.gz"
+#curl -o runner.tar.gz -L "https://github.com/actions/runner/releases/download/v${VERSION}/actions-runner-linux-${ARCHITECTURE}-${VERSION}.tar.gz"
+#
+## extract the runner package
+#tar -xzf runner.tar.gz
+#
+## configure the runner
+#./bin/installdependencies.sh
+#RUNNER_ALLOW_RUNASROOT="1" ./config.sh --unattended --url "${REPO_URL}" --token "${TOKEN}" --name "${NAME}"
