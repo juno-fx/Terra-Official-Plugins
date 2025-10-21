90a30783766d98226f83e0a1a6fef8feb0984aee
aldmbmtl <alex.e.hatfield@gmail.com>
2025-10-20 20:56:00 -0400

    Pushing first startup

--- plugins/github-runner/scripts/provision.sh
+++ plugins/github-runner/scripts/provision.sh
@@ -1,2 +1,8 @@
+# echo all configurations
+
+set -e
+
+echo "RUNNER_ALLOW_RUNASROOT='1' ./config.sh --unattended --url '${URL}' --token '${TOKEN}' --name '${NAME}'"
+
 # configure the runner
 ./config.sh --unattended --url "${URL}" --token "${TOKEN}" --name "${NAME}"
