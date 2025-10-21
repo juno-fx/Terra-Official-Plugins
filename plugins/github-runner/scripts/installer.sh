13b25b0bf7e63a1824426fc66a5c016f1cf155ad
aldmbmtl <alex.e.hatfield@gmail.com>
2025-10-20 20:06:26 -0400

    Pushing first startup

--- plugins/github-runner/scripts/installer.sh
+++ plugins/github-runner/scripts/installer.sh
@@ -12,8 +12,8 @@ mkdir -pv ${DESTINATION}
 cd ${DESTINATION}
 
 # download the runner package
-echo "curl -o runner.tar.gz -L https://github.com/actions/runner/releases/download/${VERSION}/actions-runner-linux-${ARCHITECTURE}-${VERSION}.tar.gz"
-curl -o runner.tar.gz -L "https://github.com/actions/runner/releases/download/${VERSION}/actions-runner-linux-${ARCHITECTURE}-${VERSION}.tar.gz"
+echo "curl -o runner.tar.gz -L https://github.com/actions/runner/releases/download/v${VERSION}/actions-runner-linux-${ARCHITECTURE}-${VERSION}.tar.gz"
+curl -o runner.tar.gz -L "https://github.com/actions/runner/releases/download/v${VERSION}/actions-runner-linux-${ARCHITECTURE}-${VERSION}.tar.gz"
 
 # extract the runner package
 tar -xzf runner.tar.gz
