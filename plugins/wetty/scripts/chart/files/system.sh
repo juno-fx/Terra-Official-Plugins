2a94ba3bab63a4f93f71ca4c9cfd5dc25459f684
Daniel Podwysocki <48068081+danielpodwysocki@users.noreply.github.com>
2025-11-25 01:03:10 +0100

    lint shell scripts in wetty

---
+++ plugins/wetty/scripts/chart/files/system.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+cd /usr/src/app
+
+addgroup -g "{{ .Values.guid }}" wettyusers
+
+adduser -D -u "{{ .Values.puid }}" -G wettyusers "{{ .Values.user }}"
+
+echo "{{ .Values.user }}:$USER_PASS" | chpasswd
+
+{{ if .Values.packages }}
+apk add {{ .Values.packages }}
+{{ end }}
+
+yarn start --base "/polaris/{{ .Values.name }}" --allow-iframe
