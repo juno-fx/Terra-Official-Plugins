14e58dc8586776f30773426f5453f7cd846c6bde
aldmbmtl <alex.e.hatfield@gmail.com>
2025-12-11 00:59:21 -0500

    Testing out installing as a service instead.

--- plugins/github-runner/scripts/entrypoint.sh
+++ plugins/github-runner/scripts/entrypoint.sh
@@ -55,8 +55,8 @@ if [ -n "${LAUNCH}" ]; then
 
     # chroot and run the run.sh script
     echo "Launching GitHub Actions Runner..."
-    chroot /host/ bash -c "cd /${DESTINATION}/${NAME} && RUNNER_ALLOW_RUNASROOT='1' ./svc.sh install root && RUNNER_ALLOW_RUNASROOT='1' ./svc.sh start"
-
+    chroot /host/ bash -c "cd /${DESTINATION}/${NAME} && RUNNER_ALLOW_RUNASROOT='1' ./svc.sh install root" || echo "Service seems to already be installed...continuing."
+    chroot /host/ bash -c "cd /${DESTINATION}/${NAME} && RUNNER_ALLOW_RUNASROOT='1' ./svc.sh start"
     echo "GitHub Actions Runner launched."
     sleep infinity
     exit 0
