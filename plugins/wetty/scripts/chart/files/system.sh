d88a95d89a9d4528c5a90369a8ddab075e178ff7
dwalters <dwalters@junovfx.com>
2026-03-23 17:49:50 -0400

    deleting default node user for wetty

--- plugins/wetty/scripts/chart/files/system.sh
+++ plugins/wetty/scripts/chart/files/system.sh
@@ -3,6 +3,9 @@ set -e
 
 cd /usr/src/app
 
+# delete default node user to avoid uid issues for users/groups with uids of 1000
+deluser --remove-home node || echo "user node doesn't exist"
+
 addgroup -g "$PGID" wettyusers || echo "group already exists"
 
 adduser -D -u "$PUID" -G wettyusers "$USER" || echo "user already exists"
