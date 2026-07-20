25dc9c2b1334eb5c549dc69729212c27b54b1104
aldmbmtl <alex.e.hatfield@gmail.com>
2026-07-20 19:30:03 -0400

    Adding in Boinc

--- plugins/boinc/scripts/chart/files/start.sh
+++ plugins/boinc/scripts/chart/files/start.sh
@@ -7,7 +7,7 @@ set -e
 
 # Write cc_config with CPU limit from cgroup
 if [ -f /sys/fs/cgroup/cpu.max ]; then
-  NCPUS=$(awk '{int($1/100000)}' /sys/fs/cgroup/cpu.max 2>/dev/null || nproc)
+  NCPUS=$(awk '{print int($1/100000)}' /sys/fs/cgroup/cpu.max 2>/dev/null || nproc)
 else
   NCPUS=$(nproc)
 fi
@@ -25,8 +25,10 @@ s6-setuidgid abc /usr/bin/boinc --dir /config &
 while true; do
   GUI_PASS=$(cat /config/gui_rpc_auth.cfg 2>/dev/null || echo "")
   if [ -n "$GUI_PASS" ] && [ -n "$PROJECT_URL" ] && [ -n "$ACCOUNT_KEY" ]; then
-    boinccmd --passwd "$GUI_PASS" --set_global_prefs "<global_preferences><ram_max_used_busy_pct>80</ram_max_used_busy_pct><ram_max_used_idle_pct>80</ram_max_used_idle_pct></global_preferences>" 2>/dev/null
-    boinccmd --passwd "$GUI_PASS" --project_attach "$PROJECT_URL" "$ACCOUNT_KEY" 2>/dev/null && break
+    boinccmd --passwd "$GUI_PASS" --project_attach "$PROJECT_URL" "$ACCOUNT_KEY" 2>/dev/null && {
+      boinccmd --passwd "$GUI_PASS" --set_global_prefs "<global_preferences><ram_max_used_busy_pct>80</ram_max_used_busy_pct><ram_max_used_idle_pct>80</ram_max_used_idle_pct></global_preferences>" 2>/dev/null || true
+      break
+    }
   fi
   sleep 0.5
 done
